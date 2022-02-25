39a4f4880c66d18c1cfb4d65e7a54345c3e49b9a
Renate <renate.mariarmb@gmail.com>
2022-02-25 13:14:04 +0200

    first commit

---
+++ src/pkg1/Main.java
@@ -0,0 +1,18 @@
+package pkg1;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i=0;
+		int s=0;
+		
+		int v[]= {0,3,-2,1,0,-2};
+		Utils u;
+		s=Utils.functionCount(v, i);
+		System.out.println(s);
+
+	}
+
+}
